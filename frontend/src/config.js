d898bc1e5deef65946470dd9f310401029b1fe30
venkatesh peta <85110131+DigiStackOps@users.noreply.github.com>
2025-08-18 18:27:37 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // Backend API base path for reverse proxy setup.
 // In reverse proxy mode, we just hit /api which nginx will forward to backend private IP.
-const apiBaseUrl = '/api';
+const apiBaseUrl = 'http://<Backend-Public-IP>:8080/';
 export default { apiBaseUrl };
